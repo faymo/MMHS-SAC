c14059602afd96894ef9002bf763f612f8f69e09
Fayez Mohammed <fayez.hm6@gmail.com>
2024-02-15 22:23:10 -0500

    upd 6.3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/MMHS-SAC">
       <App />
     </BrowserRouter>
   </React.StrictMode>
